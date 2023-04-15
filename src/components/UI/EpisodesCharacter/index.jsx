4e3952cf92ace2a56fad256d74ba047d87a7d503
Drimodaren <men.a.a@yandex.ru>
2023-04-15 17:35:42 +0400

    #18_1 change Episodes in card character

--- src/components/UI/EpisodesCharacter/index.jsx
+++ src/components/UI/EpisodesCharacter/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
 
-export default function EpisodesCharacter({id,name,episode,air_date, fake}) {
-  return (
-    <div>{fake}</div>
-  )
+export default function EpisodesCharacter({ id, name, episode, air_date, fake }) {
+    const lastUrl = fake.slice(-1);
+    return <div>{lastUrl}</div>;
 }
