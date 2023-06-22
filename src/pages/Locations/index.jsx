ce7daada76f9b659898d518a0988b6397a4a71f9
Drimodaren <men.a.a@yandex.ru>
2023-06-22 15:22:27 +0400

    #32 fix bug with Locations and locations

---
+++ src/pages/Locations/index.jsx
@@ -0,0 +1,44 @@
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Spinner } from "components/UI/Spinner";
+import { LOADING_STATE } from "store/locations/constans";
+import style from "./Locations.module.scss";
+import image from "images/Lockation_Main.png";
+import LoadMore from "components/UI/LoadMore";
+import { loadLocations, loadMoreLocations } from "store/locations/actions";
+import { getErrors, getLoading, getLocationsAllIds } from "store/locations/selectors";
+import LocationCard from "./LocationCard";
+import FilterLocations from "./Filters";
+import { NavLink } from "react-router-dom";
+export default function Locations() {
+    const dispatch = useDispatch();
+    const locationsIds = useSelector(getLocationsAllIds);
+    const loading = useSelector(getLoading);
+    const error = useSelector(getErrors);
+    const randomID = Math.floor(Math.random() * (100 - 1)) * 1;
+    useEffect(() => {
+        dispatch(loadLocations());
+    }, [dispatch]);
+    if (error) {
+        return <div>{error}</div>;
+    }
+    return (
+        <div className={style.locations}>
+            <img src={image} alt="Locations" />
+            <NavLink to={`${randomID}`}>
+                <p>"let's go in and out Morty, 20 minute adventure"</p>
+            </NavLink>
+            <div>
+                <FilterLocations />
+            </div>
+            <div className={style.locationsCards}>
+                {locationsIds.map(item => (
+                    <LocationCard id={item} key={item} />
+                ))}
+                {loading === LOADING_STATE.LOADING && <Spinner />}
+            </div>
+            <LoadMore loadData={loadMoreLocations()} />
+        </div>
+    );
+}
