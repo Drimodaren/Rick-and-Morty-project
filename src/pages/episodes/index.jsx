83eaade996380746946897391fa7b125956156af
Drimodaren <men.a.a@yandex.ru>
2023-06-30 18:29:43 +0400

    38 fucking case

--- src/pages/episodes/index.jsx
+++ src/pages/episodes/index.jsx
@@ -1,29 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadEpisodes } from "store/episodes/actions";
-import { getEpisodesByAllIds, getErrors } from "store/episodes/selectors";
-import EpisodeCard from "./EpisodeCard";
-import Filter from "./Filter";
 
-export default function Episodes() {
-    const episodesId = useSelector(getEpisodesByAllIds);
-    const error = useSelector(getErrors);
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(loadEpisodes());
-    }, [dispatch]);
-    if (error) {
-        <div>{error}</div>;
-    }
-    return (
-        <div>
-            <Filter />
-            <div>
-                {episodesId.map(item => (
-                    <EpisodeCard id={item} key={item} />
-                ))}
-            </div>
-        </div>
-    );
+
+export default function Episodes({}) {
+
+    
+    return 
 }
+
