984b91aa88a4352ffffae22c41af8ea48f53123c
Drimodaren <men.a.a@yandex.ru>
2023-07-06 17:15:33 +0400

    51 redesign singleCharacter

--- src/components/UI/Quotes/index.jsx
+++ src/components/UI/Quotes/index.jsx
@@ -5,15 +5,22 @@ export default function Quotes({ image }) {
     const [quote, setQuote] = useState("");
     const [loading, setLoading] = useState(true);
     const cb = async () => {
-        const response = await fetch("http://loremricksum.com/api/");
-        const text = await response.json();
-        setQuote(text.data[0]);
+        if (window.location.protocol === "http:") {
+            const response = await fetch("http://loremricksum.com/api/");
+            const text = await response.json();
+            setQuote(text.data[0]);
+        } else {
+            const response = await fetch(`${process.env.PUBLIC_URL}/rick-and-morty-quotes.json`);
+            const arr = await response.json();
+            const randomID = Math.floor(Math.random() * (arr.length - 1)) * 1;
+            setQuote(arr[randomID]);
+        }
+
         setLoading(false);
     };
     useEffect(() => {
         cb();
     }, []);
-   
 
     return (
         <div className={style.quote} onClick={cb}>
